92d74d988fa3dc9f1ab7a39ae1036e7bbe5bb0e7
Amos Kipyegon <amoskip@kabarak.ac.ke>
2022-06-19 22:13:33 +0300

    Bump(UI): user landing page and configs

---
+++ store-vue-ui/src/components/Layouts/TheTopBar.vue
@@ -0,0 +1,84 @@
+<template>
+    <header class="w-full h-full flex justify-between items-center px-4 pt-2 md:px-12 shadow-sm">
+        <nav class="right flex gap-4 h-full items-center py-2">
+            <div class="logo relative h-full flex justify-center items-center gap-2 ml-6">
+                <IconEcosystemVue class="icon text-blue-400"/>
+                <h3 class="text-xl font-extrabold ml-4">Store</h3>
+            </div>
+            <div class="links flex gap-6 items-center justify-start h-full ml-2 relative pt-2 md:ml-4">
+                <RouterLink to="/" class="ml-4 text-sm flex items-center px-2 h-full">Home</RouterLink>
+                <RouterLink to="/" class="ml-4 text-sm flex items-center px-2 h-full">Products</RouterLink>
+                <RouterLink to="/" class="ml-4 text-sm flex items-center px-2 h-full">Sell</RouterLink>
+                <RouterLink to="/" class="ml-4 text-sm flex items-center px-2 h-full">Admin</RouterLink>
+            </div>
+             <!-- hover:border-b-2 hover:border-blue-500 hover:duration-300 -->
+        </nav>
+        <nav class="left flex h-full justify-center items-center gap-2 pt-2">
+            <button class=" p-2">Login</button>
+            <button class=" rounded-xl px-2 py-1 outline outline-1  outline-offset-1 hover:outline-double hover:outline-offset-2">Sign up</button>
+        </nav>
+    </header>
+
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import IconEcosystemVue from '../icons/IconEcosystem.vue';
+
+</script>
+
+<style scoped>
+
+a {
+    transition-property: border;
+    position: relative;
+}
+.icon {
+    transform: scale(1.5);
+}
+@keyframes hov {
+    0% {
+        width: 0%;
+    }
+    /* 25% {
+        width: 25%;
+    } */
+    50% {
+        width: 50%;
+    }
+    /* 75% {
+        width: 75%;
+    } */
+    100% {
+        width: 100%;
+    }
+}
+a::after {
+    position: absolute;
+    content: '';
+    bottom: 0%;
+    left: 0%;
+    width: 100%;
+    height: 0.15rem;
+    background: rgb(59 130 246);
+    visibility: hidden;
+}
+a:hover::after {
+    visibility: visible;
+    animation: hov 200ms;
+}
+.logo {
+    padding: 2px 0;
+    margin-top: 4px;
+}
+.links::before {
+    position: absolute;
+    content: '';
+    left: 0;
+    top: 0.25rem;
+    width: 2px;
+    height: 50%;
+    transform: translate(0%, 50%);
+    background: #c0c0c0;
+}
+</style>
--- store-vue-ui/src/views/HomeView.vue
+++ store-vue-ui/src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import TheTopBarVue from '@/components/Layouts/TheTopBar.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="w-full h-full flex flex-col">
+    <section class="header w-full h-16 bg-red">
+      <TheTopBarVue />
+    </section>
+    <section class="w-full flex flex-row p-4 mt-8">
+      <div class="left w-1/2 flex flex-col gap-4 mt-8">
+        <h2 class="w-full h-4 text-green-400 flex items-center relative">CREATIVE DATA ANALYSIS</h2>
+        <h3 class=" ">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorum laborum, excepturi repellat quae.</h3>
+      </div>
+      <div class="right w-1/2">
+        <img src="@/assets/retail.jpeg" alt="" class=" rounded-md object-cover w-full contrast-125">
+      </div>
+    </section>
   </main>
 </template>
+
+<style scoped>
+
+</style>
